7e34ec4c71e75c283ce73338226902bd22aa16fc
MatiasPP96 <matiaas.p@gmail.com>
2024-12-13 11:34:49 -0300

    feat: cleaning agent directory

--- backend/src/internal/config/index.ts
+++ backend/src/internal/config/index.ts
@@ -7,6 +7,7 @@ export const PLAYHT_API_KEY = process.env.PLAYHT_API_KEY;
 export const PLAYHT_USER_ID = process.env.PLAYHT_USER_ID;
 export const AGENT_API_URL = process.env.AGENT_API_URL;
 export const AGENT_ID = process.env.AGENT_ID;
+export const LIVEPEER_API_KEY = process.env.LIVEPEER_API_KEY;
 
 function stopProgram(envKey: string) {
   console.error(`no ${envKey} specified in enviroment variable`);
@@ -23,4 +24,5 @@ export function validateRequiredEnvs() {
   if (!PLAYHT_USER_ID) stopProgram("PLAYHT_USER_ID");
   if (!AGENT_API_URL) stopProgram("AGENT_API_URL");
   if (!AGENT_ID) stopProgram("AGENT_ID");
+  if (!LIVEPEER_API_KEY) stopProgram("LIVEPEER_API_KEY");
 }
