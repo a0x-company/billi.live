21c798b597acda4915e47613f0fb9d7c1186c7e7
tesslaray <cristian.valdivia@alumnos.usm.cl>
2024-12-13 01:58:25 -0300

    feat: return pub hash in livestreams storage

--- backend/src/internal/livestreams/storage.ts
+++ backend/src/internal/livestreams/storage.ts
@@ -188,6 +188,7 @@ export class LivestreamStorage {
           streamedByAgent: data.streamedByAgent,
           cast: data.cast,
           pfpUrl: data.pfpUrl,
+          pubHash: data.pubHash,
         };
       }
 
